1d7a0633f18cce19d91eb199395a9e6d30dfbb5b
sergiokulchytsky <serhiyx16@gmail.com>
2015-03-03 20:39:01 +0200

    Described class Connection

--- pzas32driver/FileConnection.cs
+++ pzas32driver/FileConnection.cs
@@ -7,23 +7,45 @@ using System.Threading.Tasks;
 
 namespace pzas32driver
 {
+    /// <summary>
+    /// This class organizes connection to the file. </summary>
     public class FileConnection
     {
-        string path;
+        /// <summary>
+        /// Store for the file path. </summary>
+        string path; 
+
+        /// <summary>
+        /// Using for open and close file. </summary>
         public FileStream fstream;
+
+        /// <summary>
+        /// The class constructor. </summary>
+        /// <param name="path"> Path to the file. </param>
         public FileConnection(string path)
         {
             this.path = path;
         }
+
+        /// <summary>
+        /// Method to open the file. </summary>
+        /// <param name="fmode"> Specifies how should opening a file. </param>
+        /// <param name="faccess"> Defines constants for access to a file. </param>
+        /// <returns> Return true if file exist. </returns>
         public bool Open(FileMode fmode, FileAccess faccess){
-            if(File.Exists(this.path))
+            if (File.Exists(this.path))
             {
                 this.fstream = new FileStream(this.path, fmode, faccess);
                 return true;
             }
             else
-                throw new Exception("Файл "+ this.path +" не існує");
+            {
+                throw new Exception("Файл " + this.path + " не існує");
+            }
         }
+
+        /// <summary>
+        /// Method to close the file. </summary>
         public void Close()
         {
             this.fstream.Close();
