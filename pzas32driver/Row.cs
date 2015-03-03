81f76a6ff66ef989fb34746831e8298e5d1bb572
sergiokulchytsky <serhiyx16@gmail.com>
2015-03-03 21:15:52 +0200

    Described class Row

--- pzas32driver/Row.cs
+++ pzas32driver/Row.cs
@@ -6,17 +6,31 @@ using System.Threading.Tasks;
 
 namespace pzas32driver
 {
+    /// <summary>
+    /// Describe element of data model. /// </summary>
     public class Row
     {
         List<String> cols;
+
+        /// <summary>
+        /// The class constructor. </summary>
+        /// <param name="cols"> List of type string items. </param>
         public Row(List<String> cols)
         {
             this.cols = cols;
         }
+
+        /// <summary>
+        /// Method to get column. </summary>
+        /// <returns> Return list of type string items</returns>
         public List<String> getCols()
         {
             return this.cols;
         }
+
+        /// <summary>
+        /// Method that convert list items to string. </summary>
+        /// <returns> Return result string </returns>
         public String ToString()
         {
             String res = "";
@@ -31,6 +45,10 @@ namespace pzas32driver
             
             return res;
         }
+
+        /// <summary>
+        /// Method to add item to the row. </summary>
+        /// <param name="col"> String with some text. </param>
         public void Add(String col)
         {
             this.cols.Add(col);
