bac4f3dda5dc54ae629362b4208d0923faed8b35
tolyan---tolyan <tolyan---tolyan@ukr.net>
2015-03-03 19:50:10 +0200

    cleared testing data

--- test1/Program.cs
+++ test1/Program.cs
@@ -13,17 +13,6 @@ namespace test1
             FileConnection fcon = new FileConnection(@"E:\1.csv");
             FileCommand fcom = new FileCommand(fcon);
             List<String> list = new List<String>();
-
-            list.Add("serhiy");
-            list.Add("098");
-            list.Add("6435541");
-            //Row row = new Row(list);
-            //fcom.update(8, row);
-            fcom.add(new Row(list));
-            //FileResultSet fset = fcom.read();
-           
-            //Console.WriteLine("Count {0}", fset.count());
-            //Console.ReadLine();
         }
     }
 }
